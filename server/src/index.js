6e264e1d8928c74c79578975104050ae231e7720
Arastu Vij <arastuvij@Arastus-MacBook-Pro.local>
2025-04-18 16:23:01 +0530

    v1 completed

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import path from "path";
+
 import {connectDB} from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import gameRoutes from "./routes/game.route.js"
@@ -12,15 +14,22 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
+const  __dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.json());    
 app.use(cookieParser());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 
-app.use("/auth", authRoutes);
-app.use("/game", gameRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/game", gameRoutes);
 
+if (process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname, "../client/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+    });
+}
 server.listen(PORT, () => {
     console.log("server running on", PORT);
     connectDB();
