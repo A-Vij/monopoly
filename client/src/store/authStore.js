f5ea1aeb00baef5590368b4e9bf6fcf5207affe7
Arastu Vij <arastuvij@Arastus-MacBook-Pro.local>
2025-04-18 17:31:38 +0530

    v1 completed

--- client/src/store/authStore.js
+++ client/src/store/authStore.js
@@ -3,6 +3,8 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:8000" : "/";
+
 export const useAuthStore = create((set, get) => ({
     
     authUser: null,
@@ -70,7 +72,7 @@ export const useAuthStore = create((set, get) => ({
         const { authUser } = get();
         if (!authUser || get().socket?.connected) return; 
 
-        const socket = io("http://localhost:8000", {
+        const socket = io(BASE_URL, {
             query: {
                 userId: authUser._id,
             },
